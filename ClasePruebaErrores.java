09ab64e31ad7f9a1a4bad2f95fb430a5cc83b7d6
José Luis Arteta Buelvas <jartetab@unicartagena.edu.co>
2025-11-07 20:35:07 -0500

    Ejercicio 3

---
+++ ClasePruebaErrores.java
@@ -0,0 +1,11 @@
+//public class ClasePruebaErrores implements Nadador, Respirador{
+//    @Override
+//    public void nadar() {
+//        System.out.println("nadar");
+//    }
+//
+//    @Override
+//    public void respirar() {
+//        System.out.println("respirar");
+//    }
+//}
